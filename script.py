7b9e1e04ac524f0ad1dac77519b2dcd64515e482
plwslw <s.lit.winkler@gmail.com>
2017-05-03 15:35:34 -0400

    dsfa

---
+++ script.py
@@ -0,0 +1,35 @@
+import mdl
+from display import *
+from matrix import *
+from draw import *
+
+def run(filename):
+    """
+    This function runs an mdl script
+    """
+    color = [255, 255, 255]
+    tmp = new_matrix()
+    ident( tmp )
+
+    p = mdl.parseFile(filename)
+
+    if p:
+        (commands, symbols) = p
+    else:
+        print "Parsing failed."
+        return
+
+    #print p
+
+    ident(tmp)
+    stack = [ [x[:] for x in tmp] ]
+    screen = new_screen()
+    tmp = []
+    step = 0.1
+    for command in commands:
+
+        print command
+        if command = 'push':
+            print "dank memes"
+
+run("test.mdl")
